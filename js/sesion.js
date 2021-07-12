2819c4259f9c20f4f0058235072e765a3d794fac
PaulaZP <mzp1405mail.com>
2021-07-12 17:55:10 -0600

    se hacen cambios

--- js/sesion.js
+++ js/sesion.js
@@ -1,22 +1,32 @@
-function addCharacters() {
-  fetch('https://kt2ul4cwza.execute-api.us-east-2.amazonaws.com/public/artists')
-    .then((response) => response.json())
-    .then((data) => {
+class GeneralPlayer{
+  constructor(data){
+    this.data = data;
+  }
+  artist(){
+    for (let i = 0; i < this.data.length; i++) {
 
-    const characterList = document.getElementById('character-list');
+      const characterList = document.getElementById('character-list');
 
-    const item = document.createElement('li');
-    characterList.appendChild(item);
+      const item = document.createElement('li');
+      item.setAttribute('class', 'item-artist');
+      characterList.appendChild(item);
 
-    const picture = document.createElement('img');
-    picture.setAttribute('src', `${data.image}`);
-    item.appendChild(picture);
+      const picture = document.createElement('img');
+      picture.setAttribute('src', `${this.data[i].image}`);
+      picture.setAttribute('class', 'picture-artist');
+      item.appendChild(picture);
 
-    const artistName = document.createElement('p');
-    artistName.setAttribute('src', `${data.name}`);
-    item.appendChild(artistName);
-
-  });
-
+      const artistName = document.createElement('p');
+      artistName.setAttribute('class', 'name-artist');
+      item.appendChild(artistName);
+      artistName.innerHTML = `${this.data[i].name}`;
+    }
+  }
 }
-addCharacters();
+
+fetch('https://kt2ul4cwza.execute-api.us-east-2.amazonaws.com/public/artists')
+.then((response) => response.json())
+.then((data) => {
+  const prueba2 = new GeneralPlayer(data);
+  prueba2.artist();
+});
